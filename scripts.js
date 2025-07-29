6435faefbb38d0a2760e4b754859c28b533f4cb0
silverwolf1223 <101886723+silverwolf1223@users.noreply.github.com>
2025-07-29 10:03:56 -0500

    added neg/pos operator and decimal

--- scripts.js
+++ scripts.js
@@ -6,72 +6,147 @@ const operators = [...document.querySelectorAll(".operator")];
 
 const equal = document.querySelector("#equals");
 
-const reset = document.querySelector("#reset");
+const clear = document.querySelector("#reset");
+
+const negative = document.querySelector("#negpos");
+
+let isNeg = false;
 
 let usedOperator = false;
 
-let num1 = 0;
+let locked = false;
 
-let num2 = 0;
+let num1 = undefined;
+
+let num2 = undefined;
 
 let operator;
 
 equal.addEventListener('click', () => equals());
 
-reset.addEventListener('click', () => {
-    num1 = 0;
-    num2 = 0;
+negative.addEventListener('click', () => switchSign());
+
+clear.addEventListener('click', () => {
+    num1 = undefined;
+    num2 = undefined;
+    isNeg = false;
     usedOperator = false;
     screen.value = "";
+    locked = false;
 })
 
 nums.forEach((num) => num.addEventListener('click', () => {
-    screen.value = screen.value + num.textContent;
-    screen.focus();
+    if(!locked)
+    {
+        screen.value = screen.value + num.textContent;
+        screen.focus();
+    }
 }));
 
 operators.forEach((op) => op.addEventListener('click', () => {
-    if(!usedOperator)
+    if(!usedOperator && !locked)
     {
         operate(op.textContent);
         screen.focus();
     }
 }));
 
-//only allows numbers so letters aren't calculated
-screen.addEventListener("keypress", (event) => {
-    const allowedChars = "0123456789.";
-    const opChars = "+-*/";
-    const key = String.fromCharCode(event.keyCode || event.which);
-    if(opChars.includes(key))
+function switchSign()
+{
+    if(!isNeg)
     {
-        operate(key);
+        screen.value = "-" + screen.value;
+        isNeg = true;
     }
-    if (!allowedChars.includes(key)) {
-        event.preventDefault();
+    else
+    {
+        screen.value = screen.value.slice(1);
+        isNeg = false;
     }
-});
+}
 
 function operate(str)
 {
-    num1 = +screen.value;
-    operator = str;
-    screen.value = "";
-    usedOperator = true;
+    if(!isNaN(num1) && screen.value !== "")
+    {
+        num2 = +screen.value;
+        screen.value = ops[operator](num1, num2);
+        num1 = screen.value;
+    }
+    else
+    {
+        num1 = +screen.value;
+        operator = str;
+        screen.value = "";
+        //usedOperator = true;
+    }
+    if(screen.value.includes("-"))
+    {
+        isNeg = true;
+    }
+    else
+    {
+        isNeg = false;
+    }
 }
 
 function equals()
 {
-    num2 = +screen.value;
+    if(screen.value !== "") num2 = +screen.value;
+    if(isNaN(num1) || isNaN(num2)) return;
     usedOperator = false;
     result = ops[operator](num1, num2);
     screen.value = result;
     screen.focus();
+    if(screen.value.includes("-"))
+    {
+        isNeg = true;
+    }
+    else
+    {
+        isNeg = false;
+    }
 }
 
 const ops = {
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
     '*': (a, b) => a * b,
-    '/': (a, b) => a / b,
-}
+    '/': (a, b) => {if(b == 0)
+        {
+            locked = true;
+            return "dumbass";
+        }
+        else
+        {
+            return a / b;
+        }},
+};
+
+//only allows numbers so letters aren't calculated
+screen.addEventListener("keypress", (event) => {
+    const allowedChars = "0123456789";
+    const period = ".";
+    const opChars = "+-*/";
+    const eqsign = "=";
+    const key = String.fromCharCode(event.keyCode || event.which);
+    if(opChars.includes(key))
+    {
+        operate(key);
+    }
+    if(eqsign.includes(key)||event.key === "Enter")
+    {
+        equals();
+    }
+    if(period.includes(key) && screen.value.includes(period))
+    {
+        event.preventDefault();
+    }
+    else if (!allowedChars.includes(key) && !period.includes(key)) {
+        event.preventDefault();
+    }
+    if(locked)
+    {
+        event.preventDefault();
+    }
+});
